8ea7ec0a35379b8e0ad9688fe3517281541270b9
soroosh-gb <soroosh.gb@gmail.com>
2020-08-07 13:51:56 -0400

    soloution

--- lib/project.rb
+++ lib/project.rb
@@ -1,16 +1,24 @@
 class Project
+    attr_accessor :backer
     attr_reader :title;
+
+   
 
     def initialize(title)
         @title = title;
+        
     end
+
+   
+
     #add_backer is an instance method that accepts a backer as an argument and creates a ProjectBacker, associating the backer with this project
     def add_backer(backer)
-        #do something
+        ProjectBacker.new(self, backer)
     end
     #backers returns an array of backers associated with this Project instance
     def backers
-        #do something
+        pbacker = ProjectBacker.all.filter {|backer| backer.project == self}
+        pbacker.map {|pb| pb.backer}
     end
 
 end
